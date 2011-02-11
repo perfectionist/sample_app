91ea8d562cf19b4c15513617ac2892bef716b96c
David Loeffler <loeffler@mac.com>
2011-02-10 21:20:39 -0600

    Added a User factory and tests for the show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ SampleApp::Application.routes.draw do
   
   resources :users
 
-  match '/signup', :to => 'users#new'
-
   match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
  
   # The priority is based upon order of creation:
   # first created -> highest priority.
